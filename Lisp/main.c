5952be1c42c6f05f3a14b38bd255e71cb4ca7607
Stan Stevens <sstevens404@gmail.com>
2015-10-14 19:31:22 -0400

    Initial Commit

---
+++ Lisp/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lisp
+//
+//  Created by Stanford Stevens on 10/14/15.
+//  Copyright (c) 2015 Stanford Stevens. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
